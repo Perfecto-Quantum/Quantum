49c6852bf3b6200e692bbaa5fd5970129a9c8bb6
Shanehess <34279451+Shanehess@users.noreply.github.com>
2018-08-02 13:29:15 -0400

    Added the date picker selection for XCUITestPicker    - Using the RemoteWebElement    - Using the locator and creating an QAFExtendedWebElement
    Added options to interact with objects directly.
       - Tap
       - Slide
       - Get the scale of the phone

--- src/main/java/com/quantum/utils/DeviceUtils.java
+++ src/main/java/com/quantum/utils/DeviceUtils.java
@@ -10,13 +10,17 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.hamcrest.Matchers;
+import org.openqa.selenium.Rectangle;
 import org.openqa.selenium.remote.DriverCommand;
 import org.openqa.selenium.remote.RemoteExecuteMethod;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.remote.RemoteWebElement;
 
 import com.perfectomobile.httpclient.device.DeviceParameter;
 import com.perfectomobile.httpclient.device.DeviceResult;
 import com.qmetry.qaf.automation.ui.WebDriverTestBase;
 import com.qmetry.qaf.automation.ui.webdriver.QAFExtendedWebDriver;
+import com.qmetry.qaf.automation.ui.webdriver.QAFExtendedWebElement;
 import com.qmetry.qaf.automation.util.Validator;
 
 public class DeviceUtils {
@@ -492,5 +496,175 @@ public class DeviceUtils {
 
 	}
 
-
+	/**
+	 * Sets the picker wheel to the value specified.
+	 * @param picker - WebElement that holds the XCUIElementTypePicker
+	 * @param direction - Direction to spin the spinner, either next or previous defaults to next
+     * @param value - value to compare this must be exact
+	 */
+	public static void setPickerWheel(RemoteWebElement picker,String direction, String value){
+    	value = value.replaceAll("[^\\x00-\\x7F]", "");
+		String name = picker.getAttribute("value").replaceAll("[^\\x00-\\x7F]", "");
+    	while(!name.equals(value))
+    	{
+    		System.out.println(name);
+    		pickerwheelStep(picker,direction);
+    		// title based will retrieve the title as a string,
+        	// view based will retrieve a string that represent the view
+        	//(uniqueness depends on the developer of the app).
+        	name = picker.getAttribute("value").replaceAll("[^\\x00-\\x7F]", "");
+    	}
+    }
+    /**
+     * Returns the selected value from the picker wheel
+     * @param element - WebElement that holds the XCUIElementTypePicker
+     * @return string value of the value attribute
+     */
+    public static String pickerwheelGet(RemoteWebElement element) {
+	    return element.getAttribute("value");
+	}
+    /**
+     * Moves the pickerwheel in one step using a default of 0.15
+	 * @param picker - WebElement that holds the XCUIElementTypePicker
+	 * @param direction - Direction to spin the spinner, either next or previous defaults to next
+     */
+    public static void pickerwheelStep(RemoteWebElement element, String direction) {
+    	pickerwheelStep(element,direction,0.15);
+    }
+    /**
+     * Moves the pickerwheel in one step 
+	 * @param picker - WebElement that holds the XCUIElementTypePicker
+	 * @param direction - Direction to spin the spinner, either next or previous defaults to next
+     * @param offset - the offset of the picker this represents 1 slide
+     */
+    public static void pickerwheelStep(RemoteWebElement element, String direction, double offset) {
+	    Map<String, Object> params = new HashMap<>();
+	    params.put("order", direction);
+	    params.put("offset", offset);
+	    params.put("element", element.getId());
+	    getQAFDriver().executeScript("mobile: selectPickerWheelValue", params);
+	}
+    /**
+     * Sets the picker wheel to the value specified.
+	 * @param locator - Locator to find the element based on
+	 * @param direction - Direction to spin the spinner, either next or previous defaults to next
+     * @param value - value to compare this must be exact
+     */
+    public static void setPickerWheel(String locator,String direction, String value){
+    	setPickerWheel((RemoteWebElement) getQAFDriver().findElement(locator), direction, value);
+    }
+	/**
+	 * This function will calculate the location of the element on the 
+	 * device and manually tap the point location of the middle of the 
+	 * element. This function accounts that there may be a header to offset 
+	 * from.
+	 * @param loc - locator to find the element to be clicked
+	 * @param addressBar - navigation bar that takes up the top half of the 
+	 * 					   device outside of the webview
+	 */
+		public static void touchObject(String loc, String addressBar) {
+			int bannerY = getOffset(addressBar);
+			int scaleFactor = getScale();
+			//Gets the rectangle of the element we want to click
+	        Rectangle rect = new QAFExtendedWebElement(loc).getRect();
+	        //calculates the middle x value using the rectangle and multiplying the scale
+			int x = (rect.getX()+(rect.getWidth()/2))*scaleFactor;
+			//calculates the middle y value using the rectangle, adding the offset
+			//and multiplying the scale
+			int y = (rect.getY()+(rect.getHeight()/2)+bannerY)*scaleFactor;
+			//Touch the device at the point calculated
+			touch(x+","+y);	
+		}
+		/**
+		 * Slides the provided object to the left
+		 * @param loc object to slide
+		 */
+		public static void slideObjectLeft(String loc)
+		{
+			//uses 0.5 to get the middle of the Y
+			float y = 0.5f;
+			//Since we are sliding left, we want to start on the right side of the element 
+			//and end on the left side
+			float startX = (2.0f/3.0f);
+			float endX = (1.0f/3.0f);
+			//This calls the slide object using the constant values we set for 
+			//the default left slide
+			slideObject(loc, startX,endX, y);
+		}
+		/**
+		 * Slides the provided object
+		 * @param loc object to slide 
+		 * @param xStartMult - x point to start on
+		 * @param xEndMult - y point to end on
+		 * @param yMult - y point for both the start and stop
+		 */
+		public static void slideObject(String loc, float xStartMult,float xEndMult, float yMult)
+		{
+			slideObject(loc, xStartMult,xEndMult, yMult, yMult);
+		}
+		/**
+		 * 
+		 * Slides the provided object
+		 * @param loc object to slide 
+		 * @param xStartMult - x point to start on
+		 * @param xEndMult - y point to end on
+		 * @param yStartMult - y point to start on
+		 * @param yEndMult - y point to end on
+		 */
+		public static void slideObject(String loc, float xStartMult,float xEndMult, float yStartMult,float yEndMult)
+		{
+			//Gets the current scale of the device
+			int scaleFactor = getScale();
+			//Gets the rectangle of the object to use the x,y and width, height
+			Rectangle rect = new QAFExtendedWebElement(loc).getRect();
+			//Gets point to start y
+			int startY = Math.round(((rect.getY()+(rect.getHeight()*yStartMult)))*scaleFactor);
+			//Gets point to stop y
+			int endY = Math.round((rect.getY()+(rect.getHeight()*yEndMult))*scaleFactor);
+			//Gets the point to start x
+			int startX = Math.round((rect.getX()+(rect.getWidth()*xStartMult))*scaleFactor);
+			//gets the point to stop y
+			int endX = Math.round((rect.getX()+((rect.getWidth())*xEndMult))*scaleFactor);
+			//swipes using the points provided
+			swipe(startX+","+startY, endX+","+endY);
+		}
+		
+		/**
+		 * Gets the current application sacale for the device
+		 * @return integer value of scale
+		 */
+		public static int getScale() {
+			//Gets the resolution of the current device
+			String deviceRes = getDeviceProperty("resolution");
+	        //Gets the width of the root application viewport 
+			int appWidth = new QAFExtendedWebElement("xpath=/*/*").getSize().getWidth();
+	        //compares the resolution to the application dimensions to find out what the pixel scale is
+			return Math.round(Integer.parseInt(deviceRes.split("\\*")[0])/appWidth);
+		}
+		/**
+		 * Gets the offset of the header values to offset y value of the header element
+		 * @param addressBar - header element to measure
+		 * @return the y offset of the element
+		 */
+		public static int getOffset(String addressBar) {
+			return getOffset(addressBar, "NATIVE_APP");
+		}
+		/**
+		 * Gets the offset of the header values to offset y value of the header element
+		 * @param addressBar - header element to measure
+		 * @param context - context of the element to use
+		 * @return the y offset of the element
+		 */
+		public static int getOffset(String addressBar, String context) {
+			//Stores the current context so we can switch to it at the end
+			String curContext = DeviceUtils.getCurrentContext();
+			// Switch to native context
+			switchToContext(context); 
+			//Gets the rectangle of the welement to get the x,y and width height
+			Rectangle view = new QAFExtendedWebElement(addressBar).getRect();		
+			switchToContext(curContext); // Switch back to the original context
+			//this gets the application size of the area above the viewport
+			//we will use this to offset the element
+			return (view.getY()+view.getHeight());
+		}
 }
