e73c183fd7f82fd90d4e6cccc889adc109a4f17e
kulin sitwala <kulinsitwala@gmail.com>
2018-11-21 10:55:38 -0500

    Retry class updated logic with the fixes to execute all the failures when the retry is configured

--- src/main/java/com/quantum/utils/Retry.java
+++ src/main/java/com/quantum/utils/Retry.java
@@ -1,26 +1,66 @@
 package com.quantum.utils;
 
+import static com.qmetry.qaf.automation.core.ConfigurationManager.getBundle;
+import static com.qmetry.qaf.automation.util.StringUtil.toStringWithSufix;
+
+import java.util.List;
+
 import org.testng.IRetryAnalyzer;
 import org.testng.ITestResult;
 
-import com.qmetry.qaf.automation.core.ConfigurationManager;
+import com.qmetry.qaf.automation.keys.ApplicationProperties;
+import com.qmetry.qaf.automation.step.client.TestNGScenario;
 
-public class Retry implements IRetryAnalyzer  {
+public class Retry implements IRetryAnalyzer {
+	public static String RETRY_INVOCATION_COUNT = "retry.invocation.count";
 
-	int counter = 0;
-	int retryLimit = ConfigurationManager.getBundle().getInt("retry.count");
-
-	
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.testng.IRetryAnalyzer#retry(org.testng.ITestResult)
+	 */
 	@Override
 	public boolean retry(ITestResult result) {
-		
-		if (counter < retryLimit) {
-			counter++;
-			
-			return true;
+
+		boolean shouldRetry = shouldRetry(result);
+		if (shouldRetry) {
+			try {
+				if (result.getMethod() instanceof TestNGScenario) {
+					((TestNGScenario) result.getMethod()).decAndgetCurrentInvocationCount();
+				}
+			} catch (Exception e) {
+				System.err.println(e);
+			}
+			int retryInvocationCount = getRetryCount() + 1;
+			System.err.println(
+					"Retrying [" + result.getName() + "]" + toStringWithSufix(retryInvocationCount) + " time.");
+
+			getBundle().addProperty(RETRY_INVOCATION_COUNT, retryInvocationCount);
+
+			// correct failed invocation numbers for data driven test case.
+			List<Integer> failedInvocations = result.getMethod().getFailedInvocationNumbers();
+			if (null != failedInvocations && !failedInvocations.isEmpty()) {
+				int lastFailedIndex = failedInvocations.size() - 1;
+				failedInvocations.remove(lastFailedIndex);
+			}
+
+		} else {
+			getBundle().clearProperty(RETRY_INVOCATION_COUNT);
 		}
 
-		return false;
+		return shouldRetry;
+	}
 
+	public boolean shouldRetry(ITestResult result) {
+		Throwable reason = result.getThrowable();
+		int retryCount = getRetryCount();
+		boolean shouldRetry = (result.getStatus() == ITestResult.FAILURE)
+				&& (ApplicationProperties.RETRY_CNT.getIntVal(0) > retryCount);
+
+		return shouldRetry;
+	}
+
+	protected int getRetryCount() {
+		return getBundle().getInt(RETRY_INVOCATION_COUNT, 0);
 	}
 }
